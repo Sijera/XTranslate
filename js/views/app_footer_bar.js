f68e192e04f8482aaf6acce521f15f822e9d7d5a
Roman <ixrock@gmail.com>
2014-02-16 01:11:33 +0700

    minor

--- js/views/app_footer_bar.js
+++ js/views/app_footer_bar.js
@@ -1,8 +1,13 @@
 'use strict';
 
-var UTILS = require('../utils'),
-    inherit = require('../utils').inherit,
+var inherit = require('../utils').inherit,
     UIComponent = require('../ui/ui_component').UIComponent;
+
+/** @const */
+var RATE_URL = '';
+
+/** @const */
+var DONATE_URL = 'https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=NDM8RZ6PG5G6S&lc=US&item_name=XTranslate%20%28browser%20extension%29&item_number=2014&currency_code=EUR&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted';
 
 /**
  * @constructor
@@ -10,25 +15,18 @@ var UTILS = require('../utils'),
 var AppFooterBar = function (options) {
     AppFooterBar.superclass.constructor.call(this, options);
 
-    this.rateLink = '';
-    this.donateLink = '';
-    this.createDom();
+    var rateLink = this.getHtmlLink(RATE_URL, __(61));
+    var donateLink = this.getHtmlLink(DONATE_URL, __(62));
+
+    this.$container.addClass('appFooterBar');
+    this.$container.append(__(60, [rateLink, donateLink]));
 };
 
 inherit(AppFooterBar, UIComponent);
 
 /** @private */
 AppFooterBar.prototype.getHtmlLink = function (url, text) {
-    return UTILS.sprintf('<a href="{0}">{1}</a>', url, text);
-};
-
-/** @private */
-AppFooterBar.prototype.createDom = function () {
-    this.$container.addClass('appFooterBar');
-
-    var rateLink = this.getHtmlLink(this.rateLink, __(61));
-    var donateLink = this.getHtmlLink(this.donateLink, __(62));
-    this.$container.append(__(60, [rateLink, donateLink]));
+    return '<a href="' + url + '">' + text + '</a>';
 };
 
 exports.AppFooterBar = AppFooterBar;
